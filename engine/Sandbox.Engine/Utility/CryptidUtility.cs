1b12b08650dd9518a5ae3fefd7677348061e2b55
boxrocket <boxrocket6803@gmail.com>
2026-08-27 12:32:02 -0500

    add global shift key input

--- engine/Sandbox.Engine/Utility/CryptidUtility.cs
+++ engine/Sandbox.Engine/Utility/CryptidUtility.cs
@@ -1,6 +1,10 @@
-﻿namespace Sandbox;
+﻿using Sandbox.Engine;
+
+namespace Sandbox;
 
 public static class CryptidUtility {
 	public static void AddLogger( Action<LogEvent> logger ) => Logging.OnMessage += logger;
 	public static void RemoveLogger( Action<LogEvent> logger ) => Logging.OnMessage -= logger;
+
+	public static bool ShiftDown() => InputRouter.IsButtonDown(NativeEngine.ButtonCode.KEY_LSHIFT) || InputRouter.IsButtonDown(NativeEngine.ButtonCode.KEY_RSHIFT);
 }
